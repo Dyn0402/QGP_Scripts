c14318fdf1275ec84df52fa8c3ad34bc65c2353e
Dyn0402 <Dyn0402@hotmail.com>
2020-04-09 13:20:40 -0700

    Added run_ampt.py which emulates run.sh Gang gave me.

---
+++ run_ampt.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on April 09 12:59 PM 2020
+Created in PyCharm
+Created as QGP_Scripts/run_ampt.py
+
+@author: Dylan Neff, dylan
+"""
+
+import subprocess
+import sys
+
+
+def main():
+    run_id = gen_id(sys.argv[1])
+    set_run_dir(run_id)
+    print(f'Run_id: {run_id}')
+    run(run_id)
+    clean_up(run_id)
+    print('donzo')
+
+
+def gen_id(job_id):
+    run_id = str(job_id).split('_')[-1] + '1'
+    return run_id
+
+
+def set_run_dir(run_id):
+    subprocess.call(f'mkdir {run_id}')
+    subprocess.call(f'cd {run_id}')
+    subprocess.call('mkdir ana')
+    subprocess.call('cp ../input.ampt ana/')
+    subprocess.call('cp ../input.ampt .')
+    subprocess.call('cp ../makeAmptroot.C .')
+
+
+def run(run_id):
+    subprocess.call(['./ampt', str(run_id)])
+    subprocess.call('root -b -q makeAmptroot.C++')
+    subprocess.call(f'mv ana/test.root ../test_{run_id}.root')
+
+
+def clean_up(run_id):
+    subprocess.call('cd ..')
+    subprocess.call(f'rm -r {run_id}')
+
+
+if __name__ == '__main__':
+    main()
