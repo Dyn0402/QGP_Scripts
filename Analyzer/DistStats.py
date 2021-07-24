3ffbca6fd915d632e400335b16faeb906a0489fd
Dylan Neff <Dyn0402@hotmail.com>
2021-07-23 18:27:29 -0700

    DistStats.py added delta theorem errors for cumulants and copied for k-statistics

--- Analyzer/DistStats.py
+++ Analyzer/DistStats.py
@@ -174,61 +174,97 @@ class DistStats:
     def get_cumulant(self, order):
         """
         Calculate cumulant of distribution for given order from central moments with error from delta theorem
-        !!!! DELTA THEOREM ERRORS NOT YET IMPLEMENTED !!!!!
         :param order: Order of cumulant to calculate
         :return: Cumulant value and error as Measure object
         """
-        self.calc_cent_moments(1, order)
-        # Error catching for delta theorem error calc
-        err = 1
+        self.calc_cent_moments(1, 2*order)
         # Maybe write in bell polynomials later
+        cm = self.cent_moments
+        n = self.total_counts
         if order == 1:
             val = self.raw_moments[1]
+            err = (cm[2] / n) ** 0.5
         elif order == 2:
-            val = self.cent_moments[2]
+            val = cm[2]
+            err = pow((cm[4] - pow(cm[2], 2)) / n, 0.5)
         elif order == 3:
-            val = self.cent_moments[3]
+            val = cm[3]
+            err = ((cm[6] - cm[3] ** 2 + 9 * cm[2] ** 3 - 6 * cm[2] * cm[4]) / n) ** 0.5
         elif order == 4:
-            val = self.cent_moments[4] - 3 * self.cent_moments[2] ** 2
+            val = cm[4] - 3 * cm[2] ** 2
+            err = pow((cm[8] - 12 * cm[6] * cm[2] - 8 * cm[5] * cm[3] - pow(cm[4], 2) + 48 * cm[4] * pow(cm[2], 2)
+                       + 64 * pow(cm[3], 2) * cm[2] - 36 * pow(cm[2], 4)) / n, 0.5)
         elif order == 5:
-            val = self.cent_moments[5] - 10 * self.cent_moments[2] * self.cent_moments[3]
+            val = cm[5] - 10 * cm[2] * cm[3]
+            err = pow((cm[10] - pow(cm[5], 2) - 10 * cm[4] * cm[6] + 900 * pow(cm[2], 5) - 20 * cm[3] * cm[7]
+                       - 20 * cm[8] * cm[2] + 125 * cm[2] * pow(cm[4], 2) + 200 * cm[4] * pow(cm[3], 2)
+                       - 1000 * pow(cm[3] * cm[2], 2) + 160 * cm[6] * pow(cm[2], 2) - 900 * cm[4] * pow(cm[2], 3)
+                       + 240 * cm[2] * cm[3] * cm[5]) / n, 0.5)
         elif order == 6:
-            val = self.cent_moments[6] - 15 * self.cent_moments[4] * self.cent_moments[2] \
-                  - 10 * self.cent_moments[3]**2 + 30 * self.cent_moments[2]**3
+            val = cm[6] - 15 * cm[4] * cm[2] - 10 * cm[3] ** 2 + 30 * cm[2] ** 3
+            err = pow((-30 * cm[4] * cm[8] + 510 * cm[4] * cm[2] * cm[6] + 1020 * cm[4] * cm[3] * cm[5]
+                       + 405 * cm[8] * pow(cm[2], 2) - 2880 * cm[6] * pow(cm[2], 3)
+                       - 9720 * cm[3] * cm[5] * pow(cm[2], 2) - 30 * cm[2] * cm[10] + 840 * cm[2] * cm[3] * cm[7]
+                       + 216 * cm[2] * pow(cm[5], 2) - 40 * cm[3] * cm[9] + 440 * cm[6] * pow(cm[3], 2)
+                       - 3600 * pow(cm[2] * cm[4], 2) - 9600 * cm[2] * cm[4] * pow(cm[3], 2)
+                       + 13500 * cm[4] * pow(cm[2], 4) + 39600 * pow(cm[2], 3) * pow(cm[3], 2) + cm[12] - pow(cm[6], 2)
+                       - 12 * cm[5] * cm[7] + 225 * pow(cm[4], 3) - 8100 * pow(cm[2], 6)
+                       - 400 * pow(cm[3], 4)) / n, 0.5)
+        else:
+            print(f'{order}th order cumulant not implemented, returning nan!')
+            val = float('nan')
+            err = float('nan')
 
         return Measure(val, err)
 
     def get_k_stat(self, order):
         """
         Calculate k statistic of distribution for given order from central moments
-        !!!! ERRORS NOT YET IMPLEMENTED !!!!!
-        :param order: Order of k statistic to colculate
+        !!!! ERRORS ESTIMATED AS SAME AS CUMULANTS VIA DELTA THEOREM !!!!!
+        :param order: Order of k statistic to calculate
         :return: K statistic value and error as Measure object
         """
-        self.calc_cent_moments(1, order)
-        # Placeholder for actual error calculation
-        err = 1
+        self.calc_cent_moments(1, 2*order)
         # Maybe try to find analytical formula later
         n = self.total_counts
-        m = self.cent_moments
+        cm = self.cent_moments
         if n < order:  # Will get /0 errors
             print(f'Too few entries ({n}) for {order} order k-statistic!')
             return Measure(float('NaN'), float('NaN'))
         if order == 1:
             val = self.raw_moments[1]
+            err = (cm[2] / n) ** 0.5
         elif order == 2:
-            val = n / (n - 1) * m[2]
+            val = n / (n - 1) * cm[2]
+            err = pow((cm[4] - pow(cm[2], 2)) / n, 0.5)
         elif order == 3:
-            val = n ** 2 / ((n - 1) * (n - 2)) * m[3]
+            val = n ** 2 / ((n - 1) * (n - 2)) * cm[3]
+            err = ((cm[6] - cm[3] ** 2 + 9 * cm[2] ** 3 - 6 * cm[2] * cm[4]) / n) ** 0.5
         elif order == 4:
-            val = n ** 2 * ((n + 1) * m[4] - 3 * (n - 1) * m[2] ** 2) \
-                  / ((n - 1) * (n - 2) * (n - 3))
+            val = n ** 2 * ((n + 1) * cm[4] - 3 * (n - 1) * cm[2] ** 2) / ((n - 1) * (n - 2) * (n - 3))
+            err = pow((cm[8] - 12 * cm[6] * cm[2] - 8 * cm[5] * cm[3] - pow(cm[4], 2) + 48 * cm[4] * pow(cm[2], 2)
+                       + 64 * pow(cm[3], 2) * cm[2] - 36 * pow(cm[2], 4)) / n, 0.5)
         elif order == 5:
-            val = n ** 3 * ((n + 5) * m[5] - 10 * (n - 1) * m[2] * m[3]) \
-                  / ((n - 1) * (n - 2) * (n - 3) * (n - 4))
+            val = n ** 3 * ((n + 5) * cm[5] - 10 * (n - 1) * cm[2] * cm[3]) / ((n - 1) * (n - 2) * (n - 3) * (n - 4))
+            err = pow((cm[10] - pow(cm[5], 2) - 10 * cm[4] * cm[6] + 900 * pow(cm[2], 5) - 20 * cm[3] * cm[7]
+                       - 20 * cm[8] * cm[2] + 125 * cm[2] * pow(cm[4], 2) + 200 * cm[4] * pow(cm[3], 2)
+                       - 1000 * pow(cm[3] * cm[2], 2) + 160 * cm[6] * pow(cm[2], 2) - 900 * cm[4] * pow(cm[2], 3)
+                       + 240 * cm[2] * cm[3] * cm[5]) / n, 0.5)
         elif order == 6:
-            val = n ** 2 * ((n + 1) * (n ** 2 + 15 * n - 4) * m[6] - 15 * (n - 1) ** 2 * (n + 4) * m[2] * m[4] - 10 *
-                            (n - 1) * (n ** 2 - n + 4) * m[3] ** 2 + 30 * n * (n - 1) * (n - 2) * m[2] ** 3) / \
-                  ((n - 1) * (n - 2) * (n - 3) * (n - 4) * (n - 5))
+            val = n ** 2 * ((n + 1) * (n ** 2 + 15 * n - 4) * cm[6] - 15 * (n - 1) ** 2 * (n + 4) * cm[2] * cm[4]
+                            - 10 * (n - 1) * (n ** 2 - n + 4) * cm[3] ** 2 + 30 * n * (n - 1) * (n - 2) * cm[2] ** 3) \
+                  / ((n - 1) * (n - 2) * (n - 3) * (n - 4) * (n - 5))
+            err = pow((-30 * cm[4] * cm[8] + 510 * cm[4] * cm[2] * cm[6] + 1020 * cm[4] * cm[3] * cm[5]
+                       + 405 * cm[8] * pow(cm[2], 2) - 2880 * cm[6] * pow(cm[2], 3)
+                       - 9720 * cm[3] * cm[5] * pow(cm[2], 2) - 30 * cm[2] * cm[10] + 840 * cm[2] * cm[3] * cm[7]
+                       + 216 * cm[2] * pow(cm[5], 2) - 40 * cm[3] * cm[9] + 440 * cm[6] * pow(cm[3], 2)
+                       - 3600 * pow(cm[2] * cm[4], 2) - 9600 * cm[2] * cm[4] * pow(cm[3], 2)
+                       + 13500 * cm[4] * pow(cm[2], 4) + 39600 * pow(cm[2], 3) * pow(cm[3], 2) + cm[12] - pow(cm[6], 2)
+                       - 12 * cm[5] * cm[7] + 225 * pow(cm[4], 3) - 8100 * pow(cm[2], 6)
+                       - 400 * pow(cm[3], 4)) / n, 0.5)
+        else:
+            print(f'{order}th order k-statistic not implemented, returning nan!')
+            val = float('nan')
+            err = float('nan')
 
         return Measure(val, err)
