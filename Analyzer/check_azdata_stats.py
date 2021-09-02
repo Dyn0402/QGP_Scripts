23596b8400417d80fc61fc93806748087ae4c7b0
Dylan Neff <Dyn0402@hotmail.com>
2021-09-02 15:40:20 -0700

    Tweaked check_azdata_stats.py to check both pull and ratio stats

--- Analyzer/check_azdata_stats.py
+++ Analyzer/check_azdata_stats.py
@@ -23,12 +23,20 @@ def main():
     path = f'{base_path}{set_name}{set_num}/{energy}GeV/ratios_divisions_{div}_centrality_{cent}_local.txt'
 
     az_data = AzimuthBinData(div=div, path=path)
+    az_data.print_dist()
 
-    # az_data.print_dist()
+    ratio_dist = az_data.get_ratio_dist()
+    az_ratio_stats = DistStats(dist=ratio_dist)
+    print(f'Ratio: {path}')
+    print(f'mean: {az_ratio_stats.get_mean()}')
+    print(f'sd: {az_ratio_stats.get_sd()}')
+    print(f'skew: {az_ratio_stats.get_skewness()}')
+    print(f'kurt: {az_ratio_stats.get_kurtosis()}')
+    print(f'kurt*var: {az_ratio_stats.get_kurt_var()}')
+
     pull_dist = az_data.get_pull_dist()
-
     az_pull_stats = DistStats(dist=pull_dist)
-    print(path)
+    print(f'Pull: {path}')
     print(f'mean: {az_pull_stats.get_mean()}')
     print(f'sd: {az_pull_stats.get_sd()}')
     print(f'skew: {az_pull_stats.get_skewness()}')
