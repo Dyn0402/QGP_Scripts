6b966f3f322cb81dcacf1564335145f0939d2a63
Dylan Neff <Dyn0402@hotmail.com>
2021-10-01 20:50:58 -0700

    Added music_fist_net_protons.py to reproduce cumulants.
    Not started yet, need to generate CF data with anti-protons

---
+++ Music_Fist_Reproduce/music_fist_net_protons.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on September 29 3:20 PM 2021
+Created in PyCharm
+Created as QGP_Scripts/music_fist_net_protons
+
+@author: Dylan Neff, dylan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
