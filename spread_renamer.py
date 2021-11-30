9ba10bb4af648f22973545e74c3227ef06de1d9b
Dylan Neff <Dyn0402@hotmail.com>
2021-11-30 00:03:05 -0800

    spread_renamer.py fixed Old and New PC files

--- spread_renamer.py
+++ spread_renamer.py
@@ -17,23 +17,23 @@ import shutil
 def main():
     amps = ['_amp01_', '_amp02_', '_amp03_', '_amp04_']
     spread = '_spread01'
-    base_path = '/home/dylan/Research/Data_Sim_Mix/'
+    base_path = '/home/dylan/Research/Data_Sim/'
     for path in os.listdir(base_path):
         dir_path = base_path + path
-        good = False
-        for amp in amps:
-            if amp in path and spread in path:
-                good = True
-                break
+        good = True
+        # for amp in amps:
+        #     if amp in path and spread in path:
+        #         good = True
+        #         break
         if good:
             if os.path.isdir(dir_path):
                 for path2 in os.listdir(dir_path):
                     dir_path2 = dir_path + '/' + path2
                     if os.path.isdir(dir_path2):
-                        dir_path2_new = dir_path + '/' + path2.replace('spread0', 'spread')
+                        dir_path2_new = dir_path + '/' + path2.replace('spread', 'spread0')
                         print(f'{dir_path2} --> \n{dir_path2_new}')
                         shutil.move(dir_path2, dir_path2_new)
-            dir_path_new = base_path + path.replace('spread0', 'spread')
+            dir_path_new = base_path + path.replace('spread', 'spread0')
             print(f'{dir_path} --> \n{dir_path_new}')
             shutil.move(dir_path, dir_path_new)
     print('donzo')
