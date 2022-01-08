3a96d9a93acef197cac30275eb025f5b1368e305
Dyn0402 <Dyn0402@hotmail.com>
2022-01-08 00:02:27 -0800

    Added interp_test.py to figure out why interpolation comes out flipped

---
+++ Binom_Slices/interp_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on January 07 9:40 PM 2022
+Created in PyCharm
+Created as QGP_Scripts/interp_test
+
+@author: Dylan Neff, Dyn04
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate as interp
+
+
+def main():
+    x = np.linspace(0, 10, 10)
+    y = np.linspace()
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
