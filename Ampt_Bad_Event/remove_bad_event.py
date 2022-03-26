63adde48f3c2e9ffcedd45c4d3a403ac0605b8e5
Dylan Neff <Dyn0402@hotmail.com>
2022-03-26 16:34:49 -0700

    remove_bad_event.py set to clean Gang's 7GeV data. Seems to have worked.

--- Ampt_Bad_Event/remove_bad_event.py
+++ Ampt_Bad_Event/remove_bad_event.py
@@ -16,14 +16,15 @@ import shutil
 
 
 def main():
-    bad_file_list_path = '/home/dylan/Research/Ampt_Bad_Event/bad_ampt_events_slimcentral.txt'
+    bad_file_list_path = '/home/dylan/Research/Ampt_Bad_Event/bad_ampt_events_gang7GeV.txt'
     bad_tree_repo = '/home/dylan/Research/Ampt_Bad_Event/'
     bad_tree_sufx = '_bad'
     fix_tree_sufx = '_fix'
     min_identical = 2
     bad_trees = get_bad_event_file(bad_file_list_path, min_identical)
     for tree_path, tree in bad_trees.items():
-        # tree_path.replace('/media/ucla/Research/AMPT_Trees', '/gpfs01/star/pwg/dneff/data/AMPT')
+        # tree_path = tree_path.replace('/media/ucla/Research/AMPT_Trees', '/gpfs01/star/pwg/dneff/data/AMPT')
+        tree_path = tree_path.replace('D:/Research', '/media/ucla/Research')
         print(tree_path, tree)
         repo_tree_path = move_tree(tree_path, bad_tree_repo, bad_tree_sufx)
         fix_tree_path = fix_tree(tree, repo_tree_path, bad_tree_sufx, fix_tree_sufx)
