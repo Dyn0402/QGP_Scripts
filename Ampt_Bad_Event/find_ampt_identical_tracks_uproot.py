0bd17ce6f8b9bc1d9dc9ba4789cc00aee344d149
Dylan <Dyn0402@hotmail.com>
2022-03-26 17:17:33 -0700

    Set ampt_bad_event to replace bad files on rcf for gang 7GeV

--- Ampt_Bad_Event/find_ampt_identical_tracks_uproot.py
+++ Ampt_Bad_Event/find_ampt_identical_tracks_uproot.py
@@ -57,15 +57,15 @@ def uproot_finder():
     start = datetime.now()
     print(f'Start {start}\n')
 
-    out_file_path = '/home/dylan/Research/Ampt_Bad_Event/bad_ampt_events_slim_most_central_new.txt'
-    # out_file_path = 'D:/Research/Ampt_Bad_Event/bad_ampt_events_min_bias_new.txt'
+    # out_file_path = '/home/dylan/Research/Ampt_Bad_Event/bad_ampt_events_slim_most_central_new.txt'
+    out_file_path = 'D:/Research/Ampt_Bad_Event/bad_ampt_events_gang7GeV_check.txt'
     # out_file_path = '/star/u/dneff/Ampt_Bad_Event/bad_ampt_events_central.txt'
-    path = '/media/ucla/Research/AMPT_Trees/slim_most_central/string_melting/'
-    # path = 'D:/Research/AMPT_Trees/slim_most_central/string_melting/'
+    # path = '/media/ucla/Research/AMPT_Trees/slim_most_central/string_melting/'
+    path = 'D:/Research/AMPT_Trees/gang/'
     # path = '/gpfs01/star/pwg/dneff/data/AMPT/slim_most_central/string_melting/7GeV/'
-    threads = 10
+    threads = 14
     tree_name = 'tree'
-    write_mode = 'a'
+    write_mode = 'w'
     max_eta = 1
     ignore_pids = [313, 111]
     track_attributes = ['pid', 'px', 'py', 'pz']
