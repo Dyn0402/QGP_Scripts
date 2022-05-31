81629ef9ac72b93f2299085f4e1fe6e6f4f799e3
Dylan <Dyn0402@hotmail.com>
2022-05-31 16:01:22 -0700

    find_ampt_identical_tracks_uproot.py set to run new ampt model, not single event mode

--- Ampt_Bad_Event/find_ampt_identical_tracks_uproot.py
+++ Ampt_Bad_Event/find_ampt_identical_tracks_uproot.py
@@ -58,13 +58,13 @@ def uproot_finder():
     print(f'Start {start}\n')
 
     # out_file_path = '/home/dylan/Research/Ampt_Bad_Event/bad_ampt_events_slim_most_central_new.txt'
-    out_file_path = 'D:/Research/Ampt_Bad_Event/bad_ampt_events_gang2TeV.txt'
+    out_file_path = 'F:/Research/Ampt_Bad_Event/bad_ampt_events_minbias_new.txt'
     # out_file_path = '/star/u/dneff/Ampt_Bad_Event/bad_ampt_events_central.txt'
     # path = '/media/ucla/Research/AMPT_Trees/slim_most_central/string_melting/'
-    path = 'D:/Research/AMPT_Trees/gang/2-7TeV_PbPb/'
+    path = 'F:/Research/AMPT_Trees/min_bias/'
     # path = '/gpfs01/star/pwg/dneff/data/AMPT/slim_most_central/string_melting/7GeV/'
     threads = 14
-    single_events = True
+    single_events = False
     tree_name = 'tree'
     write_mode = 'w'
     max_eta = 1
