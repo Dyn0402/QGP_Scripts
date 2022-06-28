398c4445ddf31ebf81dd53b9b98d578fba104ef9
Dyn0402 <Dyn0402@hotmail.com>
2022-06-27 18:07:44 -0700

    Added job_manager.py, find_ident_tracks_rcf.py and clean_sub.xml to run cleaning on rcf
    Not finished, just a quick test of basic functionality

---
+++ Ampt_Bad_Event/job_manager.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on June 27 3:48 PM 2022
+Created in PyCharm
+Created as QGP_Scripts/job_manager
+
+@author: Dylan Neff, Dyn04
+"""
+
+import os
+
+
+def main():
+    """
+    Run from /star/u/dneff/Ampt_Bad_Event/sub
+    Submit jobs to find bad Ampt files and fix them
+    :return:
+    """
+    top_path = '/gpfs01/star/pwg/dneff/data/AMPT/most_central/'
+    file_list_name = 'root_files.txt'
+    sub_name = 'clean_sub.xml'
+    init_sub(top_path, file_list_name, sub_name)
+    print('donzo')
+
+
+def init_sub(top_path, file_list_name, sub_name):
+    gen_file_list(top_path, file_list_name)
+    submit_job(sub_name)
+
+
+def gen_file_list(top_path, file_list_name):
+    files = get_files(top_path)
+    with open(file_list_name, 'w') as file_list:
+        file_list.writelines('\n'.join(files))
+
+
+def submit_job(sub_name):
+    os.system(f'star-submit {sub_name}')
+
+
+def get_files(path):
+    """
+    Get all root paths recursively from path
+    :param path: Top level to find root paths
+    :return: List of root file paths
+    """
+    root_paths = []
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            if '.root' not in file_name:
+                continue
+            root_path = os.path.join(root, file_name)
+            root_paths.append(root_path)
+
+    return root_paths[:10]
+
+
+if __name__ == '__main__':
+    main()
