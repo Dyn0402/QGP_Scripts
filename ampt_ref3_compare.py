643d9c93f7afdb7e379123b0ee6de69cb997a01c
Dylan <Dyn0402@hotmail.com>
2022-07-01 13:36:59 -0700

    Added ampt_ref3_compare.py to compare ref3 dists for all AMPT versions, didn't get far into it

---
+++ ampt_ref3_compare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on June 28 8:01 PM 2022
+Created in PyCharm
+Created as QGP_Scripts/ampt_ref3_compare.py
+
+@author: Dylan Neff, Dylan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import uproot
+
+
+def main():
+    base_path = 'F:/Research/'
+    ampt_version_paths = {'New_Coalescence': 'Ampt_Centralities',
+                          'Baryon_First': 'Ampt_Centralities_Baryon_First',
+                          'Meson_First': 'Ampt_Centralities_Meson_First'}
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
