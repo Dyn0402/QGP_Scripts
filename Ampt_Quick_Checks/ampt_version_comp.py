7c379a4172e539448aacbc32308e1489d827df0d
Dyn0402 <Dyn0402@hotmail.com>
2022-07-20 07:37:20 -0700

    ampt_version_comp.py added option to run proton distributions, not tested

--- Ampt_Quick_Checks/ampt_version_comp.py
+++ Ampt_Quick_Checks/ampt_version_comp.py
@@ -11,7 +11,10 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 import uproot
+import awkward as ak
+import vector
 
 from multiprocessing import Pool
 import tqdm
@@ -45,14 +48,23 @@ def main():
     }
     mid_path = '/string_melting/'
     energies = [7]
-    tree_name = 'tree'
-    tree_attribute = 'imp'
-    # bins = np.arange(-0.5, 430.5, 1)
-    bins = np.linspace(-0.1, 25, 200)
-    density = True
-    y_log = False
+
     num_files = None  # None for all
     threads = 8
+
+    tree_name = 'tree'
+    tree_attribute = 'imp'
+
+    # job_func = read_file_att
+    # job_pars = [tree_name, tree_attribute]
+    # bins = np.arange(-0.5, 430.5, 1)
+
+    job_func = read_file_protons
+    job_pars = [tree_name, tree_attribute, 2212, 5, 0.5]
+    bins = np.linspace(-0.1, 25, 200)
+
+    density = True
+    y_log = False
 
     ampt_version_dists = {}
 
@@ -62,16 +74,17 @@ def main():
         fig.canvas.manager.set_window_title(f'{energy}GeV {tree_attribute}')
         for v_name, v_path in ampt_version_paths.items():
             print(f'Starting {v_name}')
-            ref3_dist = np.array([])
+            dist = np.array([])
             path = f'{base_path}{v_path}{mid_path}{energy}GeV/'
-            jobs = [(f'{path}{root_name}', tree_name, tree_attribute) for root_name in os.listdir(path)[:num_files]]
+            jobs = [(f'{path}{root_name}', *job_pars) for root_name in os.listdir(path)[:num_files]]
             with Pool(threads) as pool:
-                for file_ref3 in tqdm.tqdm(pool.istarmap(read_file_ref3, jobs), total=len(jobs)):
-                    ref3_dist = np.append(ref3_dist, file_ref3)
-            ampt_version_dists.update({v_name: np.histogram(ref3_dist, bins=bins)})
-            ax.hist(ref3_dist, bins=bins, density=density, histtype='step', alpha=0.8, label=v_name)
+                for file_att in tqdm.tqdm(pool.istarmap(job_func, jobs), total=len(jobs)):
+                    dist = np.append(dist, file_att)
+            ampt_version_dists.update({v_name: np.histogram(dist, bins=bins)})
+            ax.hist(dist, bins=bins, density=density, histtype='step', alpha=0.8, label=v_name)
         if y_log:
             ax.set_yscale('log')
+        ax.set_xlabel(tree_attribute)
         ax.legend()
         fig.tight_layout()
 
@@ -80,10 +93,24 @@ def main():
     print('donzo')
 
 
-def read_file_ref3(root_path, tree_name, tree_attribute):
+def read_file_att(root_path, tree_name, tree_attribute):
     with uproot.open(root_path) as file:
-        file_ref3 = file[tree_name].arrays(tree_attribute, library='np')[tree_attribute]
-    return file_ref3
+        file_att = file[tree_name].arrays(tree_attribute, library='np')[tree_attribute]
+    return file_att
+
+
+def read_file_protons(root_path, tree_name, track_attributes, proton_pid=2212, b_max=20, eta_max=0.5):
+    vector.register_awkward()
+    with uproot.open(root_path) as file:
+        tracks = file[tree_name].arrays(track_attributes)
+        imps = file[tree_name].arrays(['imp'])
+        tracks = tracks if b_max is None else tracks[imps.imp < b_max]
+        tracks = tracks[tracks.pid == proton_pid]
+        tracks = ak.zip({'px': tracks['px'], 'py': tracks['py'], 'pz': tracks['pz']}, with_name='Momentum3D')
+        tracks = tracks if eta_max is None else tracks[abs(tracks.eta) < eta_max]
+        proton_counts = ak.count(tracks.px, axis=1)
+
+    return proton_counts
 
 
 if __name__ == '__main__':
