e59e7bfb7f5f05e0c12e8527de52669ec791f9b4
Dyn0402 <Dyn0402@hotmail.com>
2022-07-25 18:43:09 -0700

    job_manager.py fixed a bug in combine_outputs in stripping rather than removing prefix
    Hopefully fixes the .roott issue

--- Ampt_Bad_Event/job_manager.py
+++ Ampt_Bad_Event/job_manager.py
@@ -156,7 +156,7 @@ def combine_outputs(output_path, out_combo_path, flag, list_path):
             out_read = out_file.read()
         bad_events = out_read.split(flag)[0].strip().split('\n')
         for event in bad_events:
-            temp_path = [x.strip('path: ') for x in event.split('\t') if 'path: ' in x]
+            temp_path = [x.removeprefix('path: ') for x in event.split('\t') if 'path: ' in x]
             if len(temp_path) == 1:
                 real_path = convert_files(temp_path, list_path)[0]
                 out_combo_lines.append(event.replace(temp_path[0], real_path))
