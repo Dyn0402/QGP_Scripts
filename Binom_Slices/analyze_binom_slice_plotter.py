cd4d62e8450ef05419879b6247dedfac68da24ab
Dyn0402 <Dyn0402@hotmail.com>
2022-10-17 19:04:12 -0700

    Added analyze_binom_slice_plotter.py to split somewhat messy usage of analyze_binom_slices.py methods

---
+++ Binom_Slices/analyze_binom_slice_plotter.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on October 17 4:27 PM 2022
+Created in PyCharm
+Created as QGP_Scripts/analyze_binom_slice_plotter
+
+@author: Dylan Neff, Dyn04
+"""
+
+from analyze_binom_slices import *
+
+
+def main():
+    plot_data()
+    print('donzo')
+
+
+def plot_data():
+    plt.rcParams["figure.figsize"] = (6.66, 5)
+    plt.rcParams["figure.dpi"] = 144
+    base_path = 'D:/Transfer/Research/Results/Azimuth_Analysis/'
+    df_name = 'binom_slice_stats_cent8_sim_test.csv'
+    df_path = base_path + df_name
+    sim_sets = []
+
+    amps = ['002', '004', '006', '008', '01']  # ['002', '006', '01']
+    spreads = ['05', '1']
+    for amp in amps:
+        for spread in spreads:
+            sim_sets.append(f'sim_aclmul_amp{amp}_spread{spread}')
+            sim_sets.append(f'sim_clmul_amp{amp}_spread{spread}')
+    sim_sets = sorted(sim_sets, reverse=True)
+    sim_sets = sim_sets[:int(len(sim_sets) / 2)] + sorted(sim_sets[int(len(sim_sets) / 2):])
+
+    stat_plot = 'standard deviation'  # 'standard deviation', 'skewness', 'non-excess kurtosis'
+    div_plt = 120
+    exclude_divs = [356]  # [60, 72, 89, 90, 180, 240, 270, 288, 300, 356]
+    total_protons_plt = 20
+    cent_plt = 8
+    energies_plt = [39, 'sim']  # [7, 11, 19, 27, 39, 62, 'sim']  # [7, 11, 19, 27, 39, 62]
+    # energies_fit = [7, 11, 19, 27, 39, 62]  # , 11, 19, 27, 39, 62]
+    energies_fit = ['sim', 7]
+    energy_plt = 62
+    data_types_plt = ['divide']
+    data_type_plt = 'divide'
+    samples = 72  # For title purposes only
+
+    data_sets_plt, data_sets_colors, data_sets_labels = [], None, None
+    data_sets_plt = []
+    data_sets_labels = {}
+    for sim_set in sim_sets:
+        label = ''
+        if '_clmul_' in sim_set:
+            label += 'Attractive '
+        elif '_aclmul_' in sim_set:
+            label += 'Repulsive '
+        amp, spread = get_name_amp_spread(sim_set)
+        label += f'A={amp} σ={spread}'
+        data_sets_labels.update({sim_set: label})
+
+    all_sets_plt = data_sets_plt + sim_sets[:]
+
+    df = pd.read_csv(df_path)
+    df = df.dropna()
+    print(pd.unique(df['name']))
+
+    df['energy'] = df.apply(lambda row: 'sim' if 'sim_' in row['name'] else row['energy'], axis=1)
+
+    stat_vs_protons(df, stat_plot, div_plt, cent_plt, [7, 'sim'], data_types_plt, all_sets_plt, plot=True, fit=False,
+                    data_sets_colors=data_sets_colors, data_sets_labels=data_sets_labels)
+
+    protons_fits = []
+    for div in np.setdiff1d(np.unique(df['divs']), exclude_divs):  # All divs except excluded
+        print(f'Div {div}')
+        protons_fits_div = stat_vs_protons(df, stat_plot, div, cent_plt, energies_fit, data_types_plt, all_sets_plt,
+                                           plot=False, fit=True)
+        protons_fits.append(protons_fits_div)
+    protons_fits = pd.concat(protons_fits, ignore_index=True)
+    print(protons_fits)
+    print(pd.unique(protons_fits['amp']))
+    print(pd.unique(protons_fits['spread']))
+    df_fits = plot_protons_fits_divs(protons_fits, all_sets_plt, data_sets_colors=data_sets_colors, fit=True,
+                                     data_sets_labels=data_sets_labels)
+    # print(df_fits)
+    plot_slope_div_fits(df_fits, data_sets_colors, data_sets_labels)
+    # plot_slope_div_fits_simpars(df_fits)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
