1ade97a1693a628f8b56fb75ee0a89aae20f7f63
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-02-20 16:59:23 -0800

    ampt_v2.py now gets n harmonics of rp. Set to run from 2 to 6

--- Ampt_Quick_Checks/ampt_v2.py
+++ Ampt_Quick_Checks/ampt_v2.py
@@ -34,16 +34,17 @@ def calculate_v2():
     data_path = f'{base_path}AMPT_Trees_New_Coalescence/min_bias/string_melting/'
     ampt_cent_path = f'{base_path}Ampt_Centralities_New_Coalescence/string_melting/'
     out_dir = f'{base_path}Data_Ampt_New_Coal/default_resample_epbins1/' \
-              f'Ampt_rapid05_resample_norotate_epbins1_0/'
-    out_dir = None
+              f'Ampt_rapid05_resample_norotate_epbins1_test_0/'
+    # out_dir = None
     plot_out_dir = 'F:/Research/Results/AMPT_New_Coal_QA_Plots/Flow/'
     # plot_out_dir = None
     energies = [7, 11, 19, 27, 39, 62]
+    energies = [7]
     max_rapid = 0.5
     min_pt = 0.4  # GeV
     max_pt = 2.0  # GeV
     max_p = 3.0  # GeV
-    eta_gap = 0.4  # Was 0.2 in first run
+    eta_gap = 0.2  # Was 0.2 in first run
     proton_pid = 2212
     pids = [211, 321, -211, -321, 2212, -2212]
     # pids = [2212]
@@ -51,10 +52,12 @@ def calculate_v2():
     # cents = [8]
     cent_map = {8: '0-5%', 7: '5-10%', 6: '10-20%', 5: '20-30%', 4: '30-40%', 3: '40-50%', 2: '60-70%', 1: '70-80%',
                 0: '80-90%', -1: '90-100%'}
-    calc_quantities = {'v2_ep_sum': 0, 'v2_ep_sum2': 0, 'v2_rp_sum': 0, 'v2_rp_sum2': 0, 'n_v2': 0,
-                       'ep_res_sum': 0, 'ep_res_sum2': 0, 'n_psi': 0}
+    calc_quantities = {'v2_ep_sum': 0, 'v2_ep_sum2': 0, 'n_v2': 0, 'ep_res_sum': 0, 'ep_res_sum2': 0, 'n_psi': 0}
+    rp_harmonics = [2, 3, 4, 5, 6]
+    for n in rp_harmonics:
+        calc_quantities.update({f'v{n}_rp_sum': 0, f'v{n}_rp_sum2': 0})
     read_branches = ['pid', 'px', 'py', 'pz', 'refmult3']
-    threads = 11
+    threads = 8
 
     for energy in energies:
         print(f'Starting {energy}GeV')
@@ -64,7 +67,7 @@ def calculate_v2():
         ref3_edges = get_ampt_ref3_edges(ampt_cent_path, energy)
 
         jobs = [(f'{file_dir}{path}', read_branches, cents, pids, ref3_edges, max_rapid, min_pt, max_pt, max_p, eta_gap,
-                 calc_quantities) for path in file_paths]
+                 calc_quantities, rp_harmonics) for path in file_paths[:20]]
 
         v2_data = {pid: {cent: calc_quantities.copy() for cent in cents} for pid in pids}
         with Pool(threads) as pool:
@@ -75,7 +78,12 @@ def calculate_v2():
                             v2_data[pid][cent][quant] += file_v2_data[pid][cent][quant]
 
         for pid in pids:
-            v2_avgs, v2_sds, v2_avg_err, v2_rp_avgs, v2_rp_sds, v2_rp_avg_err, cent_labels = [], [], [], [], [], [], []
+            v2_avgs, v2_sds, v2_avg_err, cent_labels = [], [], [], []
+            vn_rp_avgs, vn_rp_sds, vn_rp_avg_err = {}, {}, {}
+            for n in rp_harmonics:
+                vn_rp_avgs.update({n: []})
+                vn_rp_sds.update({n: []})
+                vn_rp_avg_err.update({n: []})
             reses, res_err = [], []
             for cent in cents:
                 data = v2_data[pid][cent]
@@ -90,10 +98,11 @@ def calculate_v2():
                 v2_avgs.append(data['v2_ep_sum'] / data['n_v2'] / res)
                 v2_sds.append(np.sqrt(data['v2_ep_sum2'] / data['n_v2'] - v2_avgs[-1] ** 2) / res)
                 v2_avg_err.append(v2_sds[-1] / np.sqrt(data['n_v2']))
-                v2_rp_avgs.append(data['v2_rp_sum'] / data['n_v2'])
-                v2_rp_sds.append(
-                    np.sqrt(data['v2_rp_sum2'] / data['n_v2'] - v2_rp_avgs[-1] ** 2) / res)
-                v2_rp_avg_err.append(v2_rp_sds[-1] / np.sqrt(data['n_v2']))
+                for n in rp_harmonics:
+                    vn_rp_avgs[n].append(data[f'v{n}_rp_sum'] / data['n_v2'])
+                    vn_rp_sds[n].append(
+                        np.sqrt(data[f'v{n}_rp_sum2'] / data['n_v2'] - vn_rp_avgs[n][-1] ** 2))
+                    vn_rp_avg_err[n].append(vn_rp_sds[n][-1] / np.sqrt(data['n_v2']))
                 cent_labels.append(cent_map[cent])
 
             if plot_out_dir:
@@ -102,7 +111,7 @@ def calculate_v2():
                 ax.axhline(0, color='black', ls='--')
                 ax.errorbar(cent_labels, v2_avgs, yerr=v2_avg_err, ls='none', color='blue', marker='o',
                             label='Event Plane')
-                ax.errorbar(cent_labels, v2_rp_avgs, yerr=v2_rp_avg_err, ls='none', color='red', marker='o',
+                ax.errorbar(cent_labels, vn_rp_avgs[2], yerr=vn_rp_avg_err[2], ls='none', color='red', marker='o',
                             label='Reaction Plane')
 
                 ax.set_ylabel('v2')
@@ -116,15 +125,34 @@ def calculate_v2():
                 ax.set_ylim(bottom=-0.01, top=0.1)
                 fig.savefig(f'{plot_out_dir}Ampt_{energy}GeV_pid_{pid}_v2_zoom.png')
 
+                for n in rp_harmonics:
+                    if n == 2:
+                        continue
+                    fig, ax = plt.subplots(dpi=144)
+                    ax.grid()
+                    ax.axhline(0, color='black', ls='--')
+                    ax.errorbar(cent_labels, vn_rp_avgs[n], yerr=vn_rp_avg_err[n], ls='none', color='red', marker='o',
+                                label='Reaction Plane')
+
+                    ax.set_ylabel(f'v{n}')
+                    ax.set_xlabel('Centrality')
+                    plt.xticks(rotation=30)
+                    ax.legend()
+                    ax.set_title(f'AMPT {energy} GeV PID {pid} v{n} Flow')
+                    fig.tight_layout()
+
+                    fig.savefig(f'{plot_out_dir}Ampt_{energy}GeV_pid_{pid}_v{n}.png')
+
             if out_dir and pid == proton_pid:
-                out_path = f'{out_dir}{energy}GeV/v2_04gap.txt'
-                write_v2(out_path, cents, v2_avgs, v2_avg_err, reses, res_err)
-                out_path_rp = f'{out_dir}{energy}GeV/v2_rp_04gap.txt'
-                write_v2(out_path_rp, cents, v2_rp_avgs, v2_rp_avg_err, reses, res_err)
+                out_path = f'{out_dir}{energy}GeV/v2.txt'
+                write_vn(out_path, cents, v2_avgs, v2_avg_err, reses, res_err, n=2)
+                for n in rp_harmonics:
+                    out_path_rp = f'{out_dir}{energy}GeV/v{n}_rp.txt'
+                    write_v2(out_path_rp, cents, vn_rp_avgs[n], vn_rp_avg_err[n], reses, res_err)
 
 
 def read_file(file_path, read_branches, cents, pids, ref3_edges, max_rapid, min_pt, max_pt, max_p, eta_gap,
-              calc_quantities):
+              calc_quantities, rp_harmonics):
     vector.register_awkward()
     v2_data = {pid: {cent: calc_quantities.copy() for cent in cents} for pid in pids}
     # data_nproton = {cent: {n: {'sum': 0, 'sum2': 0, 'n': 0} for n in range(100)} for cent in cents}
@@ -167,17 +195,19 @@ def read_file(file_path, read_branches, cents, pids, ref3_edges, max_rapid, min_
                 protons_east = protons[protons.eta >= 0].phi
                 v2_west = np.cos(2 * (protons_west - psi_east))
                 v2_east = np.cos(2 * (protons_east - psi_west))
-                v2_rp = np.cos(2 * protons.phi)  # AMPT reaction plane always at 0
                 v2 = ak.concatenate([v2_east, v2_west], axis=1)
 
                 v2_data[pid][cent]['v2_ep_sum'] = ak.sum(v2)
                 v2_data[pid][cent]['v2_ep_sum2'] = ak.sum(v2 ** 2)
-                v2_data[pid][cent]['v2_rp_sum'] = ak.sum(v2_rp)
-                v2_data[pid][cent]['v2_rp_sum2'] = ak.sum(v2_rp ** 2)
                 v2_data[pid][cent]['n_v2'] = ak.count(v2)
                 v2_data[pid][cent]['ep_res_sum'] = ak.sum(psi_res)
                 v2_data[pid][cent]['ep_res_sum2'] = ak.sum(psi_res ** 2)
                 v2_data[pid][cent]['n_psi'] = ak.count(psi_res)
+
+                for n in rp_harmonics:
+                    vn_rp = np.cos(n * protons.phi)  # AMPT reaction plane always at 0
+                    v2_data[pid][cent][f'v{n}_rp_sum'] = ak.sum(vn_rp)
+                    v2_data[pid][cent][f'v{n}_rp_sum2'] = ak.sum(vn_rp ** 2)
 
     return v2_data
 
@@ -187,6 +217,13 @@ def write_v2(out_path, cents, v2s, v2_errs, reses, res_errs):
         file.write('centrality\tv2\tresolution\n')
         for cent, v2, v2_err, res, res_err in zip(cents, v2s, v2_errs, reses, res_errs):
             file.write(f'{cent}\t{v2} {v2_err}\t{res} {res_err}\n')
+
+
+def write_vn(out_path, cents, vns, vn_errs, reses, res_errs, n=2):
+    with open(out_path, 'w') as file:
+        file.write(f'centrality\tv{n}\tresolution\n')
+        for cent, vn, vn_err, res, res_err in zip(cents, vns, vn_errs, reses, res_errs):
+            file.write(f'{cent}\t{vn} {vn_err}\t{res} {res_err}\n')
 
 
 def rapidity(px, py, pz, m):
