231e4a9f85b3db05894ae12309bb5972f6295309
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-03-22 17:57:49 -0700

    submit_cfsampler.py cd to xml_dir before star-submit
    Keep sched files in xml directory

--- CooperFryeSampler/submit_cfsampler.py
+++ CooperFryeSampler/submit_cfsampler.py
@@ -18,19 +18,21 @@ def main():
     run_types = {'CooperFrye_all': 'input.AuAu.7.7.C0-5',
                  'CooperFrye_EV_all': 'input.AuAu.7.7.C0-5.EVHRG',
                  'CooperFrye_EVb342_all': 'input.AuAu.7.7.C0-5.EVHRG_b342'}
+    template_dir = '/star/u/dneff/git/CooperFryeSamplerRunner/subs/'
+    xml_dir = '/star/u/dneff/gpfs/data/CooperFrye/gen_subs/'
 
     for energy in energies:
         for output_dir, input_template in run_types.items():
             replacements = {'CooperFrye_protons': output_dir,
                             'nProcesses="200"': f'nProcesses="{n_jobs}"',
                             'input.AuAu.7.7.C0-5.EVHRG': input_template}
-            template_path = f'/star/u/dneff/git/CooperFryeSamplerRunner/subs/sub{energy}.xml'
-            out_path = f'/star/u/dneff/gpfs/data/CooperFrye/gen_subs/sub_{output_dir}_{energy}GeV.xml'
+            template_path = f'{template_dir}sub{energy}.xml'
+            xml_path = f'{xml_dir}sub_{output_dir}_{energy}GeV.xml'
 
-            gen_xml(template_path, out_path, replacements)
+            gen_xml(template_path, xml_path, replacements)
 
             if submit:
-                os.system(f'star-submit {out_path}')
+                os.system(f'cd {xml_dir}; star-submit {xml_path}')
 
     print('donzo')
 
