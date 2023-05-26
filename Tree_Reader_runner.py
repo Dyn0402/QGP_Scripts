c17069b616731b39c7030fa32240b91fa4be0179
Dylan Neff <Dyn0402@hotmail.com>
2023-05-25 22:01:09 -0700

    Tree_Reader_runner.py quick fix for bug found in Tree_Reader.
    Set to run 62 and 39 while gone.

--- Tree_Reader_runner.py
+++ Tree_Reader_runner.py
@@ -12,6 +12,7 @@ import os
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
+from time import sleep
 
 
 def main():
@@ -23,93 +24,172 @@ def run_jobs():
     exe_path = '/home/dylan/git/Research/QGP_Fluctuations/Tree_Reader/Release/Tree_Reader'
     root_lib_path = '/home/dylan/Software/root/lib'
 
-    energies = [62]
+    energies = [62, 39]
 
     jobs = {
-        # 'BES1_def_nofileshuffle_test': {
-        #     'default_test': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_nomix_': [0, 0]
-        #     }
-        # },
-        # 'BES1_def_nofileshuffle': {
-        #     'default': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_dca08': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca08_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_dca12': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca12_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_nsprx09': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx09_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_nsprx11': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx11_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_nhfit15': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit15_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_nhfit25': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit25_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_m2r8': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r8_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_m2r4': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r4_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_dca05': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca05_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_dca15': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca15_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        'BES1_sys_nofileshuffle_nsprx075': {
-            'default_sys': {
-                'rapid05_resample_norotate_seed_dca1_nsprx075_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-            }
+        62: {
+            'BES1_def_nofileshuffle': {
+                'default': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca08': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca08_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca12': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca12_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx09': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx09_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx11': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx11_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit15_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit25': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit25_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r8': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r8_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r4': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r4_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca05': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca05_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca15_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx075': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx075_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx125': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx125_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r2': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r2_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r10': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r10_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
         },
-        # 'BES1_sys_nofileshuffle_nsprx125': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx125_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        # 'BES1_sys_nofileshuffle_m2r2': {
-        #     'default_sys': {
-        #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r2_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-        #     }
-        # },
-        'BES1_sys_nofileshuffle_m2r10': {
-            'default_sys': {
-                'rapid05_resample_norotate_seed_dca1_nsprx1_m2r10_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
-            }
+        39: {
+            # 'BES1_def_nofileshuffle_test': {
+            #     'default_test': {
+            #         'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_nomix_': [0, 0]
+            #     }
+            # },
+            'BES1_def_nofileshuffle': {
+                'default': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca08': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca08_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca12': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca12_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx09': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx09_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx11': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx11_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit15_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit25': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit25_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r8': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r8_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r4': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r4_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca05': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca05_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca15_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx075': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx075_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx125': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx125_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r2': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r2_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r10': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r10_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
         },
     }
 
     for energy in energies:
-        for job_type, job_type_dict in jobs.items():
+        for job_type, job_type_dict in jobs[energy].items():
             for set_group_name, set_group_dict in job_type_dict.items():
                 for set_name, job_nums in set_group_dict.items():
                     command = f'export LD_LIBRARY_PATH={root_lib_path};' \
@@ -118,6 +198,7 @@ def run_jobs():
                     subprocess.Popen(['gnome-terminal', '--geometry=140x20', '--', 'bash', '-c', command])
                     # # subprocess.Popen(['bash', '-i', '-c', 'source ~/.bashrc'])
                     # os.system('gnome-terminal -- bash -c "{}"'.format(command))
+        sleep(60*60*38)
 
 
 if __name__ == '__main__':
