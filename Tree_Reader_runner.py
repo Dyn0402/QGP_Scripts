f1e6f7c42ac9f03e85a0d7403f7fbcb688d5dce9
Dyn0402 <Dyn0402@hotmail.com>
2023-06-06 20:38:38 -0700

    Tree_Reader_runner.py added 11GeV and set to run only 11

--- Tree_Reader_runner.py
+++ Tree_Reader_runner.py
@@ -24,7 +24,7 @@ def run_jobs():
     exe_path = '/home/dylan/git/Research/QGP_Fluctuations/Tree_Reader/Release/Tree_Reader'
     root_lib_path = '/home/dylan/Software/root/lib'
 
-    energies = [62, 39]
+    energies = [11]
 
     jobs = {
         62: {
@@ -186,6 +186,83 @@ def run_jobs():
                 }
             },
         },
+        11: {
+            'BES1_def_nofileshuffle': {
+                'default': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca08': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca08_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca12': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca12_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx09': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx09_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx11': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx11_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit15_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nhfit25': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r6_m2s0_nhfit25_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r8': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r8_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r4': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r4_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca05': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca05_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_dca15': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca15_nsprx1_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx075': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx075_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_nsprx125': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx125_m2r6_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r2': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r2_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+            'BES1_sys_nofileshuffle_m2r10': {
+                'default_sys': {
+                    'rapid05_resample_norotate_seed_dca1_nsprx1_m2r10_m2s0_nhfit20_epbins1_calcv2_': [0, 0]
+                }
+            },
+        },
     }
 
     for energy in energies:
@@ -198,7 +275,7 @@ def run_jobs():
                     subprocess.Popen(['gnome-terminal', '--geometry=140x20', '--', 'bash', '-c', command])
                     # # subprocess.Popen(['bash', '-i', '-c', 'source ~/.bashrc'])
                     # os.system('gnome-terminal -- bash -c "{}"'.format(command))
-        sleep(60*60*38)
+        # sleep(60*60*38)
 
 
 if __name__ == '__main__':
